311f1cf321b74778beeafde22fc34ee9149efdb9
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-05-15 22:56:52 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,6 +5,8 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    
+    @resp = Faraday.get 'https://api.github.com/users/${params[:query]}/repos' do |req| 
+
+
   end
 end
